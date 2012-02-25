2c691a2a33620e8645f3e3ad635e4531cf7edcf2
fail46 <fail4602@gmail.com>
2012-02-25 02:59:52 -0500

    Moved Stop Fall hack to Movement.cpp.

--- Movement.cpp
+++ Movement.cpp
@@ -21,3 +21,15 @@ void __fastcall Hacks::GetMoveStatusHook (unsigned int This, unsigned int, int a
 
 	return;
 }
+
+void Hacks::StopFall ()
+{
+	if(WoW::IsInGame() == false || WoW::GetPlayer() == 0)
+	{
+		return;
+	}
+
+	reinterpret_cast<void (__thiscall*)(unsigned int)>(WoWBase + CMovement_C__Halt)(*reinterpret_cast<unsigned int*>(WoW::GetPlayer() + CMovement));
+	reinterpret_cast<void (__thiscall*)(unsigned int, unsigned int, unsigned int, unsigned int)>(WoWBase + CGUnit_C__OnCollideFallLandNotify)(WoW::GetPlayer(), *reinterpret_cast<unsigned int*>(reinterpret_cast<unsigned int (__cdecl*)()>(WoWBase + MovementGetGlobals)() + TimestampOffset), *reinterpret_cast<unsigned int*>(WoW::GetPlayer() + CMovement), 1);
+	return;
+}
