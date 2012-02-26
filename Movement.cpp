b74e626312366bd9878de4d49a598de5731fe32d
fail46 <fail4602@gmail.com>
2012-02-25 21:10:38 -0500

    Removed GetMovementStatus hook, because it's not needed in the new fly hack.

--- Movement.cpp
+++ Movement.cpp
@@ -1,27 +1,5 @@
 #include "OHack.hpp"
 
-void (__thiscall* Hacks::GetMoveStatusReal)(unsigned int, int, int, int);
-void __fastcall Hacks::GetMoveStatusHook (unsigned int This, unsigned int, int a1, int a2, int a3)
-{
-	if(Fly == true)
-	{
-		*reinterpret_cast<unsigned int*>(*reinterpret_cast<unsigned int*>(WoW::GetPlayer() + CMovement) + 0x38) &= ~MovementFlags::Flying;
-		if(*reinterpret_cast<unsigned int*>(*reinterpret_cast<unsigned int*>(WoW::GetPlayer() + CMovement) + 0x38) & MovementFlags::Flying)
-		{
-			Hacks::SetFly(false);
-		}
-	}
-
-	GetMoveStatusReal(This, a1, a2, a3);
-
-	if(Fly == true)
-	{
-		*reinterpret_cast<unsigned int*>(*reinterpret_cast<unsigned int*>(WoW::GetPlayer() + CMovement) + 0x38) |= MovementFlags::Flying;
-	}
-
-	return;
-}
-
 void Hacks::StopFall ()
 {
 	if(WoW::IsInGame() == false || WoW::GetPlayer() == 0)
