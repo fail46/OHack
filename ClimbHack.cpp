41d2e683511c9f0112e6f7fb5699c613f33a0be7
fail46 <fail4602@gmail.com>
2012-03-20 23:28:43 -0300

    Had a wrong address that would cause the climb hack to not work always.

--- ClimbHack.cpp
+++ ClimbHack.cpp
@@ -7,7 +7,7 @@ void Hacks::SetClimbHack (bool State)
 		Write<byte>(WoWBase + DoMovementStuff1 + 0x67D, static_cast<byte>(0xEB));
 
 		const byte ClimbHack[] = {0x90, 0x90};
-		Write(WoWBase + DoMovementStuff2 + 0x8DC, ClimbHack, 2);
+		Write(WoWBase + DoMovementStuff2 + 0x8D8, ClimbHack, 2);
 
 		UI::ClimbHack->SetChecked(GetClimbHack());
 	}
@@ -16,7 +16,7 @@ void Hacks::SetClimbHack (bool State)
 		Write<byte>(WoWBase + DoMovementStuff1 + 0x67D, static_cast<byte>(0x74));
 
 		const byte ClimbHack[] = {0x75, 0x0C};
-		Write(WoWBase + DoMovementStuff2 + 0x8DC, ClimbHack, 2);
+		Write(WoWBase + DoMovementStuff2 + 0x8D8, ClimbHack, 2);
 
 		UI::ClimbHack->SetChecked(GetClimbHack());
 	}
